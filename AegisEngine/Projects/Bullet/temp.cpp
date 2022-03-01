c61ae7bcc70399317db245e0b87bb0498d43b8d5
jonathan <jonandra@ucm.es>
2022-03-01 09:53:00 +0100

    ya va todo :')

--- AegisEngine/Projects/Bullet/temp.cpp
+++ AegisEngine/Projects/Bullet/temp.cpp
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
- 
-
-=======
->>>>>>> effc656c580893fd942cd2f08dfdb7c5976a95d5
 int main() {
 	return 0;
 }
--- AegisEngine/Src/ImGUI/temp.cpp
+++ AegisEngine/Src/ImGUI/temp.cpp
@@ -1,4 +1,4 @@
-#include "imgui.h"
+
 #include "imgui_impl_glfw.h"
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
