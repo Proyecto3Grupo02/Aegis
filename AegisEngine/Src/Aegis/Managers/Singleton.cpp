e0d4fdbd6b870ca3220ca1d237304c6d7252903c
jonathan <jonandra@ucm.es>
2022-03-10 18:56:49 +0100

    singleton

--- AegisEngine/Src/Aegis/Managers/Singleton.cpp
+++ AegisEngine/Src/Aegis/Managers/Singleton.cpp
@@ -3,15 +3,15 @@
 #include "Singleton.h"
 
 
-template<typename T>
-T* Singleton<T>::getInstance()
-{
-	if (Singleton<T>::mInstance_ == nullptr)
-	{
-		Singleton<T>::mInstance_ = new T();
-	}
-
-	return Singleton<T>::instance;
-}
+//template<typename T>
+//T* Singleton<T>::getInstance()
+//{
+//	if (Singleton<T>::mInstance_ == nullptr)
+//	{
+//		Singleton<T>::mInstance_ = new T();
+//	}
+//
+//	return Singleton<T>::instance;
+//}
 
 
--- AegisEngine/Src/Aegis/Managers/Singleton.h
+++ AegisEngine/Src/Aegis/Managers/Singleton.h
@@ -7,13 +7,14 @@ class Singleton
 
 public:
     static T* getInstance();
-   static void deleteInstance();
+    static void deleteInstance();
+
 protected:
     static T* mInstance_;
     Singleton();
     ~Singleton();
 private:
-//    //if it's called here is because there are multiple instances so we have to delete
+    //    //if it's called here is because there are multiple instances so we have to delete
     Singleton(Singleton const&) = delete; //copy
     Singleton& operator=(Singleton const&) = delete; //operator =
 
@@ -42,4 +43,15 @@ inline void Singleton<T>::deleteInstance() {
     delete Singleton<T>::mInstance_;
     Singleton<T>::mInstance_ = nullptr;
 
+}
+
+template<typename T>
+T* Singleton<T>::getInstance()
+{
+    if (Singleton<T>::mInstance_ == nullptr)
+    {
+        Singleton<T>::mInstance_ = new T();
+    }
+
+    return Singleton<T>::mInstance_;
 }
