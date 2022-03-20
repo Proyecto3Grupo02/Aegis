17a8c00109974b33c5d6227979922b8e983b1b97
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-03-20 13:53:36 +0100

    Añadido proyecto AegisScripting para usar Lua
    Ademas unas clases y una template.lua para ejemplo

---
+++ AegisEngine/Src/AegisScripting/Manager/LuaManager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef LUA_MANAGER
+#define LUA_MANAGER
+
+
+namespace Lua{
+    class LuaScript;
+}
+class LuaManager{
+public:
+    LuaManager();
+    virtual ~LuaManager();
+
+
+};
+
+
+#endif
