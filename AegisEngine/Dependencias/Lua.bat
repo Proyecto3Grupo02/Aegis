185f9a43386d169f742f5937955fbe922999863f
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-03-22 10:14:48 +0100

    Lua.bat hecho + codigo src + Lua sln

---
+++ AegisEngine/Dependencias/Lua.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set LUA_SOL= .\Lua\Lua.sln
+set LUA_LIB_PATH= .\Lua\Lua.lib
+
+if exist %LUA_LIB_PATH% goto done
+
+echo Compiling Lua...
+
+msbuild %LUA_SOL% /t:LuaProject /p:platform=x64 /p:configuration=Debug
+msbuild %LUA_SOL% /t:LuaProject /p:platform=x64 /p:configuration=Release
+
+echo Lua compiled
+
+goto end
+
+:done
+echo Lua already compiled
+
+:end
