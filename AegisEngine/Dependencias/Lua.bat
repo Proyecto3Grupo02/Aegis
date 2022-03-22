aac173b5b9cd8c6eb4506eaf28676c1a4200fbdb
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-03-22 22:11:41 +0100

    El  Lua bat funciona

--- AegisEngine/Dependencias/Lua.bat
+++ AegisEngine/Dependencias/Lua.bat
@@ -1,20 +1,25 @@
 @echo off
 
-set LUA_SOL= .\Dependencias\Lua\Lua.sln 
-set LUA_LIB_PATH= .\Dependencias\Lua\Lua.lib
+:: Batch variables
+set LUA_BUILD_SLN=.\Lua\Lua.sln
+set LUA_LIB_PATH =.\Lua\build\Lua_Project.lib
 
+:: Make the build directory
 if exist %LUA_LIB_PATH% goto done
 
+:: Build Lua
 echo Compiling Lua...
 
-msbuild %LUA_SOL% /t:LuaProject /p:platform=x64 /p:configuration=Debug
-msbuild %LUA_SOL% /t:LuaProject /p:platform=x64 /p:configuration=Release
+msbuild %LUA_BUILD_SLN% /t:Lua_Project /p:platform=x64 /p:configuration=Debug
+msbuild %LUA_BUILD_SLN% /t:Lua_Project /p:platform=x64 /p:configuration=Release 
 
 echo Lua compiled
 
 goto end
 
 :done
+
+:: Confirmation message
 echo Lua already compiled
 
 :end
