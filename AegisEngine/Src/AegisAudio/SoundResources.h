569c8ca386c059dcfc8ece9c76bcc005820f07df
Sergio Alberto Luis Cano <serluis@ucm.es>
2022-03-29 10:41:30 +0200

    continuacion de carga de recursos
    Co-Authored-By: Jorge Zurdo <18735746+Jorzuiz@users.noreply.github.com>

--- AegisEngine/Src/AegisAudio/SoundResources.h
+++ AegisEngine/Src/AegisAudio/SoundResources.h
@@ -2,8 +2,14 @@
 #ifndef SOUND_RESOURCES_H
 #define SOUND_RESOURCES_H
 
-#include <vector>
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <sstream>
+#include <map>
+#include "../AegisCommon/Managers/DebugManager.h"
+
+typedef	std::string cancion;
 
 class SoundResources
 {
@@ -12,12 +18,39 @@ public:
 	~SoundResources();
 
 private:
-	vector<string> vectorSound;
-	string ruta;
+	std::map<cancion,std::string> mapSound;
+	std::string rutaArchivo = "../../Exes/AegisEngine/x64/Assets/Audios";
 };
 
 SoundResources::SoundResources()
 {
+	std::fstream archivo;
+	archivo.open(rutaArchivo + "SoundResources.txt", std::ios::in);
+	std::string cancion;
+	
+	if (!archivo) {
+		std::cout << "El archivo no existe" << std::endl;
+		Debug()->Log("El archivo no existe");
+		std::cerr << "Error de carga de archivo de audio";
+	}
+	else {
+		int i = 0;
+		while (!archivo.eof())
+		{
+			std::string song;
+			std::string path;
+
+			getline(archivo, song);
+			getline(archivo, path);
+
+			// Añadir una comprobacion para saber si existe el archivo
+			
+			auto e = std::make_pair(song, path);
+
+			mapSound.insert(e);
+		}
+	}	
+	archivo.close();
 }
 
 SoundResources::~SoundResources()
