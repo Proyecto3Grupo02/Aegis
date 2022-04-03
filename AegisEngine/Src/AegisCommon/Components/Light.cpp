6b234cb3830b71a73f2f6e7eb81828e8f607a9cc
Amparo Rubio Bellon <ampxrubio@gmail.com>
2022-04-03 13:25:44 +0200

    Light component done

--- AegisEngine/Src/AegisCommon/Components/Light.cpp
+++ AegisEngine/Src/AegisCommon/Components/Light.cpp
@@ -1,18 +1,60 @@
-//#include "Light.h"
-//
-//Light::Light(): 
-//	AegisComponent(), mAmbient_(0,0,0), mDiffuse_(1,1,1), mSpecular_(0,0,0)
-//{
-//	ComponentManager::getInstance()->RegisterComponent<Light>("Light");
-//}
-//
-//Light::Light(Vector3 amb, Vector3 diff, Vector3 spec):
-//	AegisComponent(), mAmbient_(amb), mDiffuse_(diff), mSpecular_(spec)
-//{
-//
-//	ComponentManager::getInstance()->RegisterComponent<Light>("Light");
-//}
-//
-//Light::~Light()
-//{
-//}
+#include "Light.h"
+#include "../Entity/Entity.h"
+Light::Light(): 
+	AegisComponent(), mAmbient_(0,0,0), mDiffuse_(1,1,1), mSpecular_(0,0,0), mTr_(nullptr), mLight_(nullptr), mngr_(nullptr)
+{
+	SetDataAsInnerType(this);
+	SetComponentName("Light");
+}
+
+Light::Light(Vector3 amb, Vector3 diff, Vector3 spec, Ogre::SceneManager* sceneMng):
+	AegisComponent(), mAmbient_(amb), mDiffuse_(diff), mSpecular_(spec), mngr_(sceneMng), mTr_(nullptr), mLight_(nullptr)
+{
+	SetDataAsInnerType(this);
+	SetComponentName("Light");
+}
+
+void Light::init()
+{
+	Ogre::SceneNode* mNode = getEntity()->getNode();
+	mTr_= getEntity()->GetTransform();
+	assert(mTr_ != nullptr);
+
+	mLight_ = mngr_->createLight();
+	mNode->attachObject(mLight_);
+	renderLight();
+}
+
+void Light::renderLight()
+{
+	if (mAmbient_.x != 0.0 && mAmbient_.y != 0.0 && mAmbient_.z != 0.0) {
+		Ogre::ColourValue c;
+		c.r = mAmbient_.x;
+		c.g = mAmbient_.y;
+		c.b = mAmbient_.z;
+		mngr_->setAmbientLight(c);
+	}
+
+	mLight_->setDiffuseColour(mDiffuse_.x, mDiffuse_.y, mDiffuse_.z);
+	mLight_->setSpecularColour(mSpecular_.x, mSpecular_.y, mSpecular_.z);
+
+}
+
+void Light::ConvertToLua(lua_State* state)
+{
+	getGlobalNamespace(state).
+		beginNamespace("Aegis").
+		beginNamespace("NativeComponents").
+		deriveClass<Light, AegisComponent>("Light").
+		addProperty("ambient", &Light::getAmbient, &Light::setAmbient).
+		addProperty("diffuse", &Light::getDiffuse, &Light::setDiffuse).
+		addProperty("specular", &Light::getSpecular, &Light::setSpecular).
+		endClass().
+		endNamespace().
+		endNamespace();
+}
+
+
+Light::~Light()
+{
+}
--- AegisEngine/Src/AegisCommon/Components/Light.h
+++ AegisEngine/Src/AegisCommon/Components/Light.h
@@ -1,32 +1,43 @@
-//#pragma once 
-//
-//#ifndef LIGHT_H
-//#define LIGHT_H
-//#include "../../AegisCommon/Components/AegisComponent.h"
-//#include "../../AegisCommon/Components/Transform.h"
-//
-//#include "../../AegisCommon/Utils/Vector3.h"
-//
-//class Light: public AegisComponent
-//{
-//private:
-//    Vector3 mAmbient_;
-//    Vector3 mDiffuse_;
-//    Vector3 mSpecular_;
-//
-//    Transform* mTr_;
-//    Ogre::Light* mLight_;
-//    
-//public:
-//    Light();
-//    Light(Vector3 amb, Vector3 diff, Vector3 spec);
-//    ~Light();
-//
-//    virtual void init() override;
-//
-//};
-//
-//
-//
-//
-//#endif
+#pragma once 
+
+#ifndef LIGHT_H
+#define LIGHT_H
+#include "AegisComponent.h"
+#include "Transform.h"
+
+#include "../../AegisCommon/Utils/Vector3.h"
+
+class Light: public AegisComponent
+{
+private:
+    Vector3 mAmbient_;
+    Vector3 mDiffuse_;
+    Vector3 mSpecular_;
+
+    Transform* mTr_;
+    Ogre::Light* mLight_;
+    Ogre::SceneManager* mngr_;
+public:
+    Light();
+    Light(Vector3 amb, Vector3 diff, Vector3 spec, Ogre::SceneManager* sceneMng);
+    ~Light();
+
+    Vector3 getAmbient() { return mAmbient_; }
+    Vector3 getDiffuse() { return mDiffuse_; }
+    Vector3 getSpecular() { return mSpecular_; }
+   
+    void setAmbient(Vector3 amb) { mAmbient_ = amb; renderLight(); }
+    void setDiffuse(Vector3 diff) { mDiffuse_ = diff; renderLight(); }
+    void setSpecular(Vector3 spec) { mSpecular_ = spec; renderLight(); }
+
+    virtual void init() override;
+    void renderLight();
+
+    static void ConvertToLua(lua_State* state);
+
+};
+
+
+
+
+#endif
