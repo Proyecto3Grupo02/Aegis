01f5d8ebb455593f71e0d9bd1f10b1e260f89d18
Amparo Rubio Bellon <ampxrubio@gmail.com>
2022-04-19 16:13:40 +0200

    Sound Emitter . lua terminado

---
+++ AegisEngine/Exes/AegisEngine/x64/Assets/LuaScripts/SoundEmitter.lua
@@ -0,0 +1,11 @@
+local NAME="SoundEmitter"
+
+-- table for our functions
+local table = { }
+
+function table.GetNew(entity, args) 
+    local component = Aegis.NativeComponents.createSoundEmitter(entity,args.sound);
+    return component;
+end
+    
+return table
