0973d55be032a5cb45a76263c5051c2f37b1c2ae
Amparo Rubio Bellon <ampxrubio@gmail.com>
2022-05-05 11:00:46 +0200

    Wander kinematico en Lua a falta de probarlo y alguna correccion de fisicas

---
+++ AegisEngine/Exes/AegisEngine/x64/Assets/LuaScripts/Wander.lua
@@ -0,0 +1,50 @@
+local NAME= "Wander"
+local table={};
+function table.GetNew(entity, params)
+    local component= Aegis.CreateComponent(NAME, entity);
+    local data= component.data;
+    local transform= entity.transform;
+    local funcs = component.funcs;
+	
+
+    local rb;
+    local maxSpeed;
+
+    local rotation;
+    local velocity;
+    local maxRotation;
+    local rotTime;
+    local lastTime;
+    function Init()
+        rb= component.entity:GetComponent("Rigidbody").type;
+        lastTime=0;         
+        rotTime=5;
+
+    
+    end;
+
+    function Update(deltaTime)
+        
+        
+        velocity= maxSpeed* transform.rotation; 
+        if lastTime >rotTime then
+        rotation= RandomBinomial() * maxRotation; 
+        lastTime= rotTime;
+        rotTime=rotTime + deltaTime;
+        transform.rotation=rotation;
+        end;
+
+        rb:AddForceForward(velocity);
+
+        --Ian Millington pseudocode
+        -- result.velocity= maxSpeed* character.orientationAsVector(look vector)
+        --result.rotation=randomBinomial* maxRotation --just added a time to decide when rotate as a plus
+        --return result
+
+    end;
+
+    function RandomBinomial()
+        return math.random(-1,1) - math.random(-1,1);
+    end;
+
+end;
