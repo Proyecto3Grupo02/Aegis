b706714c19b05ec8a1fa07abc031754085f404b8
RTobar01 <rtobar01@ucm.es>
2022-06-24 21:52:08 +0200

    bat Ogre (Parte 0: Inicio)
    Seguiremos (cont.)
    
    Co-Authored-By: rubenglezortiz <61495670+rubenglezortiz@users.noreply.github.com>

---
+++ AegisEngine/Dependencias/Ogre/ogre.bat
@@ -0,0 +1,7 @@
+set BUILD_DIR=.\build
+set SOURCE_DIR=.\source
+set CMAKE_DIR=..\cmake\bin\cmake.exe
+set DEBUG_DLLS_DIR=..\..\Exes\AegisEngine\x64\Debug
+set RELEASE_DLLS_DIR=..\..\Exes\AegisEngine\x64\Release
+
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
