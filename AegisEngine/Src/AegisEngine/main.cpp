520be877427ffc0c5c8258c03da1b3f29ac191b9
Rioni <nrosa01@ucm.es>
2022-07-01 00:47:16 +0200

    Consola ya no sale en release

--- AegisEngine/Src/AegisEngine/main.cpp
+++ AegisEngine/Src/AegisEngine/main.cpp
@@ -1,24 +1,23 @@
 #include "AegisMain.h"
 #include <iostream>
 #include "checkML.h" //BASURA
+#include <Windows.h>
 
 int main() {
 	// Agrega checkML.h al proyecto e inclúyelo en todos los módulos del proyecto 
 	// Escribe el siguiente comando al inicio de la función main
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	::ShowWindow(::GetConsoleWindow(), SW_SHOW);
+#else
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 #endif // DEBUG	 
 
-	try {
-		AegisMain a;
-		if (a.init())
-			a.startGame();
-		else
-			a.free();
-	}
-	catch (std::exception& e) {
-		std::cout << e.what()<<"\n";
-	}
+	AegisMain a;
+	if (a.init())
+		a.startGame();
+	else
+		a.free();
 
 	return 0;
 }
